98b19f9c30a98f9840794c7219f10cb3ae14fd8c
lwathieu1 <lbw29@georgetown.edu>
2016-06-28 11:51:47 -0700

    Update

--- LED_Test/LED_Test.ino
+++ LED_Test/LED_Test.ino
@@ -4,6 +4,23 @@
 void setup() {
   // put your setup code here, to run once:
 
+    Update_Data Pattern1;
+    Pattern1.pattern = RAINBOW_CYCLE;
+    Pattern1.direction = FORWARD;
+    Pattern1.startTime = 0;
+    Pattern1.cycles = 5;
+    Pattern1.Index = 0;
+    Pattern1.brightness = 255;
+    Pattern1.status = false;
+    Pattern1.complete = false;
+    
+    Pattern1.group = [1,3,5,7];
+    Pattern1.groupLength = 4;
+    
+    
+    
+    
+    
 }
 
 void loop() {
