f19f48985f20a840a2721a87627e756b1abe3276
SupremeLeaderSnoke <tushar.trehon@gmail.com>
2016-06-30 17:49:18 -0700

    Update

--- testcode2.cpp
+++ testcode2.cpp
@@ -6,30 +6,6 @@
 
 using namespace std;
 
-/*struct Update_Data{
-public:
-string direction;
-string pattern;
-int startTime;
-int cycles;             //no of times the pattern should run
-int Index;
-int on_time;
-int off_time;
-int totalsteps;        //no of iterations
-int brightness;          //the current brightness of the LED's
-
-uint32_t Color1, Color2;    //required colors for the pattern
-
-unsigned long Interval;
-unsigned long lastUpdate;       //time at which the pattern was last updated
-
-bool status;            //whether or not the pattern is currently running or not
-bool complete;         //whether or not the pattern is complete or not
-
-uint16_t group[100];
-int groupLength;        //length of the group
-};
-*/
 
 
 Update_Data a;
@@ -55,20 +31,6 @@ int main()
     a.group[3] = 3;
     a.groupLength = 4;
 
-  //  b.create(a);
-/*ofstream ofile("test.ino");
-ofile<<"#include <Adafruit_NeoPixel.h>\n#include <Signaling.h>\n#include <UpdateData.h>\nSignaling Strip(16, 6, NEO_RGBW + NEO_KHZ800);\nUpdate_Data Pattern1;\nvoid setup() {\nSerial.begin(115200);\nStrip.begin();\n";
-ofile<<"Pattern1.pattern ="<<a.pattern<<";\n";
-ofile<<"Pattern1.direction ="<<a.direction<<";\n";
-ofile<<"Pattern1.Interval ="<<a.Interval<<";\n";
-ofile<<"Pattern1.lastUpdate ="<<a.lastUpdate<<";\n";
-ofile<<"Pattern1.totalsteps ="<<a.totalsteps<<";\n";
-ofile<<"Pattern1.group[0] ="<<a.group[0]<<";\n";
-ofile<<"Pattern1.group[1] ="<<a.group[1]<<";\n";
-ofile<<"Pattern1.group[2] ="<<a.group[2]<<";\n";
-ofile<<"Pattern1.group[3] ="<<a.group[3]<<";\n";
-ofile<<"Pattern1.groupLength ="<<a.groupLength<<";}\n";
-ofile<<"void loop() {\nStrip.Update(&Pattern1);\n}";
-ofile.close();*/
+    b.create(a);
 
 }
